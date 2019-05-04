e78203391ba5a7c45fd92d19ef33216d722b6f7f
Stanislav K <justtrace17@gmail.com>
2019-05-05 02:41:36 +0300

    Added drawImages() that fills canvas with 4 random images

--- lab3/page.js
+++ lab3/page.js
@@ -24,14 +24,36 @@ context.font = "bold 50px sans-serif";
 context.strokeStyle = "white";
 context.textAlign = "center";
 
-pic = new Image();
-pic.src = 
-  "https://source.unsplash.com/random/" +
-  canvas.width +
-  "x" +
-  canvas.height +
-  "?sig=" +
-  Math.floor(Math.random() * 10000);
-pic.onload = function() {
-  context.drawImage(pic, 0, 0);
+function drawImages() {
+  let pics = [];
+  for (let i = 0; i < 4; i++) {
+    pics[i] = { img: new Image() };
+  }
+
+  pics[0].x = pics[0].y = pics[1].y = pics[2].x = 0;
+  pics[2].y = pics[3].y = 325;
+  pics[1].x = pics[3].x = 750;
+
+  let count = 0;
+  for (i = 0; i < 4; i++) {
+    let pic = pics[i];
+    pic.img.src =
+      "https://source.unsplash.com/random/" +
+      750 +
+      "x" +
+      325 +
+      "?sig=" +
+      Math.floor(Math.random() * 10000);
+
+    pic.img.onload = () => {
+      count++;
+      if (count == 4) {
+        for (j = 0; j < 4; j++) {
+          context.drawImage(pics[j].img, pics[j].x, pics[j].y);
+        }
+      }
+    }
+  }
 }
+
+drawImages();
