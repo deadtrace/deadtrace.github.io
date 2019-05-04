1c25a866e652dcba5867c3d14749376bbb266bba
Stanislav K <justtrace17@gmail.com>
2019-05-05 02:07:32 +0300

    Working with canvas test

--- lab3/page.js
+++ lab3/page.js
@@ -13,3 +13,14 @@ saveButton.type = "button";
 body.appendChild(saveButton);
 
 body.appendChild(document.createElement("br"));
+
+canvas = document.createElement("canvas");
+canvas.width = 1000;
+canvas.height = 1000;
+ctx = canvas.getContext('2d');
+var img = new Image();  // Создание нового объекта изображения
+img.src = 'image.jpg';
+img.onload = function() {
+  ctx.drawImage(img, 0, 0);
+  body.appendChild(canvas);
+}
